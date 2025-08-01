7b07aad16f4abd2e970d820ed2bd32d20a573b44
Enier González Ibarra <gonzalezibarraenier@gmail.com>
2025-07-31 22:13:37 -0400

    feat(sync_ssh_config): Create files and logic layout

---
+++ sync_ssh_config.cpp
@@ -0,0 +1,18 @@
+// Parse file from .ssh/config into a data structure that fits
+//
+// Subscribe to file to check for changes
+//
+// Update datastructure in memory whenever the user makes changes
+//
+// Save a copy of file whenever you write. Keep track of changes (git?) and
+// allow the user to rollback
+//
+// Translate from that data structure to a string
+// representation that will be printed to file on change
+
+// On main thread: Make user choose if they want to keep this program's config
+// in sync with .ssh/config or on a separate .local/share/QTiSSH folder. 
+//
+// There will be a folder that would be a perfect ~/.ssh/ folder if copied 
+//
+// Keep API to main and data structures well documented 
